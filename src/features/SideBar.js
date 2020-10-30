d2c24f2d4a3b34e4d9c19b578b878c370a903ca3
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-10-30 11:51:27 -0600

    working to get NPM start to work. no luck yet

--- src/features/SideBar.js
+++ src/features/SideBar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "../style/SideBar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function SideBar() {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Jacques Rocks</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar_addChannel" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
