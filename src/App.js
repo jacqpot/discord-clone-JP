9eb5487d9e1202b6eed8738b0a65050b46268fff
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-10-30 15:05:16 -0600

    add and style sidebar

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import SideBar from "./features/SideBar";
 function App() {
   return (
     <div className="App">
-      <h1>lets build a discord clone</h1>
-
       <SideBar />
       {/* < Chat/> */}
     </div>
--- src/features/SideBar.js
+++ src/features/SideBar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "../style/SideBar.css";
+
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel";
 
 function SideBar() {
   return (
@@ -17,6 +20,11 @@ function SideBar() {
             <h4>Text Channels</h4>
           </div>
           <AddIcon className="sidebar_addChannel" />
+        </div>
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
         </div>
       </div>
     </div>
