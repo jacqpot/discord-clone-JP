f579079cf6642717d9c22081ec367dcc9c9b68fd
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-10-31 08:33:00 -0600

    user auth in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./style/App.css";
 import SideBar from "./features/SideBar";
 import Chat from "./features/Chat";
 import { selectUser } from "./features/userSlice";
 import Login from "./features/Login";
+import { auth } from "./features/firebase";
+import { login, logout } from "./features/userSlice";
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       {user ? (
--- src/features/Login.js
+++ src/features/Login.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../style/Login.css";
-import logo from "../style/logo.png";
+import { auth, provider } from "./firebase";
+import logo from "../style/Discord_logo.svg";
 import { Button } from "@material-ui/core";
 function Login() {
-  const signIn = () => {};
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
-      <h2>I am the login page</h2>
       <div className="login__logo">
         <img src={logo} alt="logo" />
       </div>
