0d833aba9410f2726cf4f5d738093859edc30951
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-10-31 10:44:53 -0600

    connect messaging to store and app done.

--- src/features/Chat.js
+++ src/features/Chat.js
@@ -10,6 +10,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "./userSlice";
 import { selectChannelId, selectChannelName } from "./appSlice";
 import db from "./firebase";
+import firebase from "firebase";
 
 function Chat() {
   const user = useSelector(selectUser);
@@ -28,13 +29,27 @@ function Chat() {
           setMessages(snapshot.docs.map((doc) => doc.data()))
         );
     }
-  }, []);
+  }, [channelId]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    db.collection("channels").doc(channelId).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user,
+    });
+    setInput("");
+  };
   return (
     <div className="chat">
       <ChatHeader channelName={channelName} />
       <div className="chat__messages">
         {messages.map((message) => (
-          <Message />
+          <Message
+            timestamp={message.timestamp}
+            message={message.message}
+            user={message.user}
+          />
         ))}
       </div>
       <div className="chat__input">
@@ -46,7 +61,12 @@ function Chat() {
             onChange={(e) => setInput(e.target.value)}
             placeholder={`Message #${channelName}`}
           />
-          <button className="chat__inputButton" type="submit">
+          <button
+            disabled={!channelId}
+            className="chat__inputButton"
+            type="submit"
+            onClick={sendMessage}
+          >
             send Message
           </button>
         </form>
--- src/features/Message.js
+++ src/features/Message.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "../style/Message.css";
 import { Avatar } from "@material-ui/core";
-function Message() {
+function Message({ timestamp, message, user }) {
   return (
     <div className="message">
-      <Avatar />
+      <Avatar src={user.photo} />
       <div className="message__info">
         <h4>
-          Jacqpot{" "}
-          <span className="message__timestamp">this is a timestamp</span>
+          {user.displayName}{" "}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
-        <p>This is a message</p>
+        <p>{message}</p>
       </div>
     </div>
   );
